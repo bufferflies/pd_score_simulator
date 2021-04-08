999d4e136a208c68b0edfcd389102ecbc9bcd35f
tongjian <1045931706@qq.com>
2021-04-08 12:29:37 +0800

    plotly
    Signed-off-by: tongjian <1045931706@qq.com>

--- scoreV2.py
+++ scoreV2.py
@@ -1,20 +1,35 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 import numpy as np
-from Iscore import IScore
+from Iscore import IScore,ControlEnum
 
 class ScoreV2(IScore):
   
-    def score(self,amp,x:np.ndarray):
+    def score(self,varibleType:ControlEnum,varible,x:np.ndarray):
         k=1
         m=256
         f=20
+        capacity=1000
+        amp=1.05
 
-        u=x/100*self.capacity
-        a=self.capacity-u
+        if varibleType==ControlEnum.capacity:
+            capacity=varible
+        elif varibleType==ControlEnum.amp:
+            amp=varible
+        elif  varibleType==ControlEnum.f:
+            f=varible  
+       
+        u=x/100*capacity
+        a=capacity-u
         r=u*amp
 
-        # score=k*r+m*(np.log(self.capacity)-np.log(a-f+1)/(u+f-1))*r
-        score=k*amp*u+m*(np.log(self.capacity)-np.log(a-f+1))/(self.capacity-a+f-1)*r
-        return  score
+        # if a>f use score2  else use score1 
+        A=np.ones(len(u))
+        A[np.where(a<f)]=0
+        
+        # Piecewise function use A
+        score1=A*(k*amp*u+m*(np.log(capacity)-np.log(a-f+1))/(capacity-a+f-1)*r)
+        score1[np.argwhere(np.isnan(score1))]=0
+        score2=(1-A)*((k+m*np.log(capacity)/capacity)*r+ (f-a)*(k+m*np.log(f)/f))
+        return  score1+score2
 
--- score_plot.py
+++ score_plot.py
@@ -1,20 +1,10 @@
-from  matplotlib import pyplot as plt
 import numpy as np
-from score import Score
 from scoreV2 import ScoreV2
+from Iscore import ControlEnum
 
+import plotly.graph_objects as go
 
-x= np.arange(50,90,0.01)
-plt.figure(1)
+x= np.arange(80,100,0.1)
 
-# generate 1 row 2 col picture 
-ax1=plt.subplot(221)
-s=Score(100,"u/c","score","used","k")
-s.plot(ax1,[100,500,1000,2000],x)
-
-ax2=plt.subplot(222)
-
-s=ScoreV2(3.22*1024,"scoreV2","score","used","amp")
-s.plot(ax2,[1.05,1.15,1.30,1.37],x)
-
-plt.show()
+s=ScoreV2("scoreV2 varies:amp","score","used ratio","amp:")
+data=s.plot([1.05,1.15,1.30,1.37],ControlEnum.amp,x)
